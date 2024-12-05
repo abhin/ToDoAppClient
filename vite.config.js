145b4ffd9d85fab05a1971abed9bf573adee39d4
abhin <abhin.pik@gmail.com>
2024-12-05 10:11:56 +0000

    vonfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Listen on all network interfaces (useful in Docker/VM environments)
+    port: 5173, // Set the port to 3000 (default is 5173)
+    strictPort: true, // Ensure the server uses the port specified, otherwise, it will exit
+    open: true, // Automatically open the app in the default browser upon starting the server
+  },
+});
